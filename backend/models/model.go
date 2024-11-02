4bd41757872bdb13a462cd583e78fc3043551029
linkaka <m13240615615@163.com>
2024-11-03 01:49:13 +0800

    updated

--- backend/models/model.go
+++ backend/models/model.go
@@ -21,7 +21,7 @@ var Upgrader = websocket.Upgrader{
 
 func init() {
 	//orm.RegisterDataBase("default", "mysql", "root:123456@tcp(127.0.0.1:3306)/test?charset=utf8")
-	orm.RegisterDataBase("default", "mysql", "root:3702200733ljj@tcp(127.0.0.1:3306)/gpt_project?charset=utf8")
+	orm.RegisterDataBase("default", "mysql", "root:3702200733Ljj@@tcp(127.0.0.1:3306)/gpt_project?charset=utf8")
 	//orm.RegisterDataBase("default", "mysql", "root:3702200733Ljj@@tcp(127.0.0.1:3306)/gpt_project?charset=utf8")
 	//orm.RegisterModel(new(Users))
 	//orm.RegisterModel()
